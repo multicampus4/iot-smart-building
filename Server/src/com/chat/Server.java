c3ca0640e1f4e2eaa35a5205790cbda72e671472
oshsage <oh12sung@naver.com>
2020-11-19 03:12:13 +0900

    Reference of Server and MainActivity

--- Server/src/com/chat/Server.java
+++ Server/src/com/chat/Server.java
@@ -13,29 +13,32 @@ import java.util.Iterator;
 import com.msg.Msg;
 
 public class Server {
-
-	int port;
-	String address;
-	String id;
+	// 멤버 변수 선언
+	int port;															// 포트
+	String address;														// IP 주소
+	String id;															// ID
 	
-	ServerSocket serverSocket;
+	ServerSocket serverSocket;											// SErverSocket 객체
 	
 	// client들의 메세지를 받는다.
-	HashMap<String, ObjectOutputStream> maps;
+	HashMap<String, ObjectOutputStream> maps;							// 해쉬맵<IP주소, 해당 아웃풋스트림>
 	
 	// sendTarget 위한 ip주소 선언
 	String targetIp = null;
 	
+	// 기본 생성자
 	public Server() {}
 	
+	// 포트를 담은 생성자
 	public Server(int port) {
 		this.port = port;
 		maps = new HashMap<>();
 	}
 	
+	// 서버를 시작하는 startServer() 함수
 	public void startServer() throws Exception {
-		serverSocket = new ServerSocket(port);
-		System.out.println("Strat Server ...");
+		serverSocket = new ServerSocket(port);							// serverSocket에 포트를 입력하여 선언
+		System.out.println("Strat Server ...");							// "서버를 시작합니다."
 		
 		// 네트워크는 스레드에서 동작시켜야 한다.
 		Runnable r = new Runnable() {
@@ -44,12 +47,13 @@ public class Server {
 			public void run() {
 				while(true) {
 					try {
-						Socket socket = null;
-						System.out.println("Ready Server ...");
-						socket = serverSocket.accept();
-						// 접속한 client들의 ip address 확인
-						System.out.println(socket.getInetAddress());
-						makeOut(socket);
+						Socket socket = null;							// 소켓 초기화
+						System.out.println("Ready Server ...");			// "서버를 준비합니다..."
+						socket = serverSocket.accept();					// 클라이언트를 기다린다.
+						
+						// 접속한 client들의 ip 주소 확인
+						System.out.println(socket.getInetAddress());	// IP 주소 출력
+						makeOut(socket);								// 각각의 client들의 outputstream을 hashmap에 저장
 						
 						// client가 들어올 때마다 새로운 스레드가 생성
 						new Receiver(socket).start();
@@ -67,37 +71,37 @@ public class Server {
 	
 	// 각각의 client들의 outputstream을 hashmap에 저장한다.
 	public void makeOut(Socket socket) throws IOException {
-		ObjectOutputStream oo;
-		oo = new ObjectOutputStream(socket.getOutputStream());
-		maps.put(socket.getInetAddress().toString(), oo);
-		System.out.println("접속자수: " + maps.size());
+		ObjectOutputStream oo;											// 아웃풋스트림 객체인 oo 선언		
+		oo = new ObjectOutputStream(socket.getOutputStream());			// 소켓으로부터 아웃풋 스트림을 가져와 대입
+		maps.put(socket.getInetAddress().toString(), oo);				// IP주소와 아웃풋스트림을 해쉬맵에 저장
+		System.out.println("접속자수: " + maps.size());					// 해쉬맵의 크기로 접속자 수를 출력
 	}
 	
 	// client들을 받는다.
 	// sendMsg를 호출하여 메세지 객체를 받는다.
+	// Receiver 쓰레드
 	class Receiver extends Thread {
-		Socket socket;
-		ObjectInputStream oi;
+		Socket socket;													// 소켓
+		ObjectInputStream oi;											// 인풋스트림
 		
+		// 소켓을 담은 Receiver 생성자
 		public Receiver(Socket socket) throws IOException {
 			this.socket = socket;
-			oi = new ObjectInputStream(this.socket.getInputStream());
+			oi = new ObjectInputStream(this.socket.getInputStream());	// 소켓으로부터 인풋스트림을 가져와 oi에 대입
 		}
 
 		@Override
 		public void run() {
 			while(oi != null) {
-				Msg msg = null;
+				Msg msg = null;											// Msg 객체인 변수 msg 초기화
 				
 				try {
-					msg = (Msg) oi.readObject();
-					System.out.println("Server.java :::" +msg);
-					if(msg.getMsg().equals("q")) {
-						// 강제로 exception을 내서 client를 삭제한다.
-						throw new Exception();
-					}else if(msg.getMsg().equals("iamAndroid")) {
-						// Hand Shake 메시지로 sendTarget 실행할 IP 저장
-						targetIp = socket.getInetAddress().toString();
+					msg = (Msg) oi.readObject();						// 인풋스트림에서 메시지 내용을 읽어와 msg에 저장
+					System.out.println("Server.java :::" +msg);			// "Server.java ::: 메시지내용"
+					if(msg.getMsg().equals("q")) {						// "q" 입력시
+						throw new Exception();							// 강제로 exception을 내서 client를 삭제한다.
+					}else if(msg.getMsg().equals("iamAndroid")) {		// "iamAndroid" 입력시
+						targetIp = socket.getInetAddress().toString();	// Hand Shake 메시지로 sendTarget 실행할 IP 저장
 					}
 					System.out.println(msg.getId() + msg.getMsg());
 //					sendMsg(msg);
@@ -108,9 +112,9 @@ public class Server {
 					if(targetIp != null)
 						sendTarget(targetIp,msg.getMsg());
 				} catch (Exception e) { // client가 갑자기 접속 중단된 경우
-					maps.remove(socket.getInetAddress().toString());
-					System.out.println(socket.getInetAddress()+".. Exited");
-					System.out.println("접속자수: " + maps.size());
+					maps.remove(socket.getInetAddress().toString());			// 해쉬맵에서 연결된 IP주소 삭제
+					System.out.println(socket.getInetAddress()+".. Exited");	// "(IP)가 나갔습니다"
+					System.out.println("접속자수: " + maps.size());				// 해쉬맵의 크기로 접속자수 출력
 					break;
 				} 
 			} // end while
@@ -136,13 +140,15 @@ public class Server {
 		sender.start();
 	}
 	
+	
+	// 특정 클라이언트에게만 메시지를 전송하는 sendTarget 함수
 	public void sendTarget(String ip, String cmd) {
-		ArrayList<String> ips = new ArrayList<String>();
-		ips.add(ip);
-		Msg msg = new Msg(ips,id,cmd);
-		Sender sender = new Sender();
-		sender.setMsg(msg);
-		new Thread(sender).start();
+		ArrayList<String> ips = new ArrayList<String>();					// IP를 담을 문자열 ArrayList 선언
+		ips.add(ip);														// ArrayList에 IP저장
+		Msg msg = new Msg(ips,id,cmd);										// IP ArrayList, ID, 메시지 내용을 담는 Msg 생성자를 이용
+		Sender sender = new Sender();										// Sender 객체 선언
+		sender.setMsg(msg);													// sender에 msg 저장
+		new Thread(sender).start();											// sender 쓰레드 실행
 	}
 	
 	// client들에게 메세지 전송한다.
@@ -159,14 +165,14 @@ public class Server {
 			while(it.hasNext()) {
 				try {
 					
-					if(msg.getIps() != null) {
-						for(String ip: msg.getIps()) {
-							maps.get(ip).writeObject(msg);
+					if(msg.getIps() != null) {								// 만약 Msg 객체 변수 중 IP Arraylist 안이 null이 아니면
+						for(String ip: msg.getIps()) {						// ips에 저장된 특정 클라이언트들만 대상으로 한다.
+							maps.get(ip).writeObject(msg);					// 해쉬맵에서 key가 "ip"인 메시지내용을 아웃풋스트림에 출력
 						}
 						break;
 					}
 					
-					it.next().writeObject(msg);
+					it.next().writeObject(msg);								// 그게 아니면 전체를 대상으로 아웃풋스트림에 메시지 내용 출력
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -177,11 +183,11 @@ public class Server {
 	
 
 	public static void main(String[] args) {
-		Server server = new Server(5253);
+		Server server = new Server(5253);									// Server 객체에  포트를 넣어 선언
 		
 		// 서버 실행
 		try {
-			server.startServer();
+			server.startServer();											// 서버 시작
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
