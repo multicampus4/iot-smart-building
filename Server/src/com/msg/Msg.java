aa2faeec0ecc26f017d92e7f3b1c7561b1f8af3b
kcloud721 <kcloud721@naver.com>
2020-11-30 15:47:23 +0900

    메시지 VO : 메시지 타입 추가(first/sensor/cmd)

--- Server/src/com/msg/Msg.java
+++ Server/src/com/msg/Msg.java
@@ -14,6 +14,7 @@ public class Msg implements Serializable {
 	private ArrayList<String> ips;
 	private String id;
 	private String msg;
+	private String type;
 	
 	public Msg() {
 	}
@@ -23,6 +24,13 @@ public class Msg implements Serializable {
 		this.msg = msg;
 	}
 	
+	public Msg(String id, String msg, String type) {
+		super();
+		this.id = id;
+		this.msg = msg;
+		this.type = type;
+	}
+
 	public Msg(ArrayList<String> ips, String id, String msg) {
 		this.ips = ips;
 		this.id = id;
@@ -35,15 +43,24 @@ public class Msg implements Serializable {
 		this.id = id;
 		this.msg = msg;
 	}
-
 	
+	public Msg(HashMap<String, Msg> maps, ArrayList<String> ips, String id, String msg, String type) {
+		super();
+		this.maps = maps;
+		this.ips = ips;
+		this.id = id;
+		this.msg = msg;
+		this.type = type;
+	}
+
 	public HashMap<String, Msg> getMaps() {
 		return maps;
 	}
+
 	public void setMaps(HashMap<String, Msg> maps) {
 		this.maps = maps;
 	}
-	
+
 	public ArrayList<String> getIps() {
 		return ips;
 	}
@@ -55,19 +72,30 @@ public class Msg implements Serializable {
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getMsg() {
 		return msg;
 	}
+
 	public void setMsg(String msg) {
 		this.msg = msg;
 	}
 
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	@Override
 	public String toString() {
-		return "Msg [maps=" + maps + ", ips=" + ips + ", id=" + id + ", msg=" + msg + "]";
+		return "Msg [maps=" + maps + ", ips=" + ips + ", id=" + id + ", msg=" + msg + ", type=" + type + "]";
 	}
 	
 	
