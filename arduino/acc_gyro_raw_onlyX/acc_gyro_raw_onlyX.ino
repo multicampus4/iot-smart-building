326a4b50931e910e007dbd8c4ea845fc989bd67d
oshsage <oh12sung@naver.com>
2020-12-03 23:23:22 +0900

    Upload Acc & Gyro Raw Data Code + Only X axis Data Code

---
+++ arduino/acc_gyro_raw_onlyX/acc_gyro_raw_onlyX.ino
@@ -0,0 +1,27 @@
+#include<Wire.h>
+
+const int MPU=0x68;  //MPU 6050 의 I2C 기본 주소
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+void setup(){
+  Wire.begin();      //Wire 라이브러리 초기화
+  Wire.beginTransmission(MPU); //MPU로 데이터 전송 시작
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     //MPU-6050 시작 모드로
+  Wire.endTransmission(true); 
+  Serial.begin(9600);
+}
+
+void loop(){
+  Wire.beginTransmission(MPU);    //데이터 전송시작
+  Wire.write(0x3B);               // register 0x3B (ACCEL_XOUT_H), 큐에 데이터 기록
+  Wire.endTransmission(false);    //연결유지
+  Wire.requestFrom(MPU,14,true);  //MPU에 데이터 요청
+  //데이터 한 바이트 씩 읽어서 반환
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  
+  //시리얼 모니터에 출력
+  Serial.print("AcX = "); Serial.println(AcX);
+ 
+  delay(500);
+}
