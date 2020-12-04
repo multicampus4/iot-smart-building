1d5c978f3a99ad6e193b92cbe50d3efd37c9d71c
kcloud721 <kcloud721@naver.com>
2020-12-04 16:45:58 +0900

    아두이노 디바이스 on/off 반영을 위한 fake변수

--- arduino/test_temper/test_temper.ino
+++ arduino/test_temper/test_temper.ino
@@ -1,6 +1,9 @@
 const int tempPin = A0;
 const int ledPin = A2;
 
+String deviceName = "";   // AIR
+String deviceAction = ""; // ON
+
 void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
@@ -8,33 +11,48 @@ void setup() {
 
 void loop() {
   delay(2000);
-  
+
+   // 온도 센서
+  int data = analogRead(tempPin);
+  float temp = ((5.0 * data) / 1024.0) * 100;
+  int fakeAmountTmp = 0;
+
   if(Serial.available()){
     String cmdMsg = "";       // cmd 예시 : AIR_ON 
-    String deviceName = "";   // AIR
-    String deviceAction = ""; // ON
     cmdMsg = Serial.readString();
     Serial.println(cmdMsg);
     Split(cmdMsg, '_');
-    Serial.println("RESULTDTT");
     Serial.println(deviceName);
     Serial.println(deviceAction);
+    switch(deviceName) {
+    case "AIR":     // 에어컨
+      switch(deviceAction){
+      case "ON":
+        fakeAmountTmp += 0.1;
+        temp -= fakeAmountTmp;
+        break;
+      case "OFF":
+        fakeAmountTmp = 0;
+        break;  
+      }
+      
+    }
 
-  } else {
-    // 평시 상태
-    Serial.print("tmp");
-    Serial.print(temp);
-    Serial.print(";hum");
-    Serial.print(temp+52.12);
-
-    Serial.print(";acx");
-    Serial.print(temp+1.01);
-    Serial.print(";acy");
-    Serial.print(temp+2.19);
-    Serial.print(";acz");
-    Serial.print(temp+1.84);
-    Serial.print(";\n");
   }
+  
+  // 평시 상태
+  Serial.print("tmp");
+  Serial.print(temp);
+  Serial.print(";hum");
+  Serial.print(temp+52.12);
+   
+  Serial.print(";acx");
+  Serial.print(temp+1.01);
+  Serial.print(";acy");
+  Serial.print(temp+2.19);
+  Serial.print(";acz");
+  Serial.print(temp+1.84);
+  Serial.print(";\n");
   
 }
 
@@ -50,11 +68,11 @@ void Split(String sData, char cSeparator) {
       if(-1 != nGetIndex) {   // 리턴된 인덱스가 있을 경우 
         sTemp = sCopy.substring(0, nGetIndex);  // 있으면 데이터 넣고
         sCopy = sCopy.substring(nGetIndex + 1); // 뺀 데이터 만큼 잘라낸다.
-        Serial.println( sTemp ); // AIR, HUM ...
+        // Serial.println( sTemp ); // AIR, HUM ...
         deviceName = sTemp;
 
       } else {  // 없으면 end
-        Serial.println( sCopy );  // ON/OFF
+        // Serial.println( sCopy );  // ON or OFF
         deviceAction = sCopy;
         break;
       }
