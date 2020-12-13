404662a24d999235a335403134958f701864665f
kcloud721 <kcloud721@naver.com>
2020-12-13 14:45:54 +0900

    SensorSwitch NFC화면구성

--- SensorSwitch/app/src/main/java/com/example/sensorswitch/Fragment3.java
+++ SensorSwitch/app/src/main/java/com/example/sensorswitch/Fragment3.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
+
 public class Fragment3 extends Fragment {
 
     public Fragment3() {
@@ -17,6 +20,19 @@ public class Fragment3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_3, container, false);
+        View view = inflater.inflate(R.layout.fragment_3, container, false);
+        LottieAnimationView animationView = view.findViewById(R.id.lottieAniView);
+        setUpAnimation(animationView);
+        return view;
     }
+
+    private void setUpAnimation(LottieAnimationView animationView) {
+        // 재생할 애니메이션 넣어준다.
+        animationView.setAnimation(R.raw.nfc_scanning2 );
+        // 반복횟수를 무한히 주고 싶을 땐 LottieDrawable.INFINITE or 원하는 횟수
+        animationView.setRepeatCount(LottieDrawable.INFINITE);
+        // 시작
+        animationView.playAnimation();
+    }
+
 }
